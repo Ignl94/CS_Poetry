51e6911a6d323da41ea2aed7dbaa7d6ae83ddf0e
Ignl94 <theenthusedwriter@gmail.com>
2020-09-30 19:39:54 -0500

    added function frames and imported module, opened txt file

---
+++ poetry.py
@@ -0,0 +1,24 @@
+''' Import module and open poetry text file '''
+from random import randint
+
+poetry = "./poetry_slam/poetry.txt"
+
+infile = open(poetry, "r")
+# -----------------------------------------------------------------------
+''' Create Functions Section '''
+
+
+def get_file_lines(filename):
+    # It should return a list of strings containing the lines of the file.
+
+
+def lines_printed_backwards(line_list):
+    # It should print the lines of the poem in reverse. Include the original line number at the beginning of each line.
+
+
+def lines_printed_random(line_list):
+    # It should print the lines of your poem in randomly order.
+
+
+def lines_printed_custom(line_list):
+    # Make sure that you carefully comment your custom function so it’s clear what it does.
